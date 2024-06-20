5e3f639e8adf7221509e0d96bdf2348b24343039
Yago Lucas <150140424+yago0202@users.noreply.github.com>
2024-06-20 11:54:52 -0300

    atividade 01

---
+++ script.js
@@ -0,0 +1,12 @@
+let numero = prompt('Digite seu número')
+
+numero = parseInt(numero)
+
+if > 0  && numero % 2 === 0) {
+    console.log(`o numero $(numero) é par`)
+}
+
+else {
+ console.log(`o numero $(numero) não é par`)
+}
+
